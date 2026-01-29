bc8f492360f4fef8f20db0137e6f5376346cf5fc
Rahul Naskar <rahul.naskar@gmail.com>
2026-01-29 13:42:56 +0530

    Added 'utils' project. Used 'utils' as a library in 'apps/backend'

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,4 +1,6 @@
 import express, {Request, Response} from 'express';
+import { addNumbers } from '@repeatmono/utils';
+
 
 const app = express();
 const port = 3000;
@@ -7,6 +9,7 @@ app.get('/', (req: Request, res: Response) => {
     res.send(`Hello`);
 })
 
-app.listen(port, () => {console.log(
-    `Express listening on port http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Express listening on port http://localhost:${port}`);
+    console.log(`Adding numbers from local package ${addNumbers(3, 4)}`);
 })
